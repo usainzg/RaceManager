f1ce58d727b4e8de82370de92cfeeec20a0880b0
Unai Sainz de la Maza Gamboa <unaihtc70@gmail.com>
2016-10-13 18:19:57 +0200

    Main GUI created.

---
+++ ProyectoCarreras/src/gui/MainWindow.java
@@ -0,0 +1,47 @@
+package gui;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class MainWindow extends JFrame{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MainWindow window = new MainWindow();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public MainWindow() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
