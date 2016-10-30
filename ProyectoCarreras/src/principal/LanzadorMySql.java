ff89783daaeba203d2552b23971781d3a50084f0
Unai Sainz de la Maza Gamboa <unaihtc70@gmail.com>
2016-10-30 13:54:54 +0100

    ModificacionUsuario view completed.

--- ProyectoCarreras/src/principal/LanzadorMySql.java
+++ ProyectoCarreras/src/principal/LanzadorMySql.java
@@ -3,20 +3,16 @@ package principal;
 import javax.swing.JFrame;
 
 import vista.MainWindow;
-import vista.ModificacionUsuario;
 
 public class LanzadorMySql {
 
 	public static void main(String[] args) {
 
-		/*MainWindow main = new MainWindow();
+		MainWindow main = new MainWindow();
 
 		main.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		main.setVisible(true);*/
+		main.setVisible(true);
 		
-		ModificacionUsuario usu = new ModificacionUsuario();
-		usu.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		usu.setVisible(true);
 
 	}
 
